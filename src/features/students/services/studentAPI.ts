ac391dffa04e1ca0f86c14ff374a58bbe021849c
bijayshrestha0817 <bijay.shrestha2@maitriservices.com>
2026-04-21 04:28:36 +0000

    catchhhh...

--- src/features/students/services/studentAPI.ts
+++ src/features/students/services/studentAPI.ts
@@ -1,6 +1,6 @@
-import wretch from "wretch";
 import type { Course } from "@/types/ICourse";
 import type { Student } from "@/types/IStudent";
+import wretch from "wretch";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_DJANGO_API_URL;
 
@@ -14,23 +14,64 @@ interface StudentResponse {
 }
 
 const GET_STUDENTS = (page: number) => {
-  return API_URL.url(`/students/?page=${page}`).get().json<StudentResponse>();
+  return API_URL.url(`/students/?page=${page}`)
+    .get()
+    .json<StudentResponse>()
+    .catch((error) => {
+      console.error("Error fetching students:", error);
+      throw error;
+    });
 };
 
-const POST_STUDENT = (data: Omit<Student, "id">) => {
-  return API_URL.url("/students/").post(data).json<Student>();
+const POST_STUDENT = (data: {
+  name: string;
+  email: string;
+  age: number;
+  course: string;
+}) => {
+  return API_URL.url("/students/")
+    .post(data)
+    .json<Student>()
+    .catch((error) => {
+      console.error("Error creating student:", error);
+      throw error;
+    });
 };
 
-const UPDATE_STUDENT = (data: Student) => {
-  return API_URL.url(`/students/${data.id}/`).put(data).json<Student>();
+const UPDATE_STUDENT = (data: {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  course: string;
+}) => {
+  return API_URL.url(`/students/${data.id}/`)
+    .put(data)
+    .json<Student>()
+    .catch((error) => {
+      console.error("Error updating student:", error);
+      throw error;
+    });
 };
 
 const DELETE_STUDENT = (data: { id: number }) => {
-  return API_URL.url(`/students/${data.id}/`).delete().res();
+  return API_URL.url(`/students/${data.id}/`)
+    .delete()
+    .res()
+    .catch((error) => {
+      console.error("Error deleting student:", error);
+      throw error;
+    });
 };
 
 const GET_COURSES_FOR_STUDENT = () => {
-  return API_URL.url(`/courses/all/`).get().json<Course[]>();
+  return API_URL.url(`/courses/all/`)
+    .get()
+    .json<Course[]>()
+    .catch((error) => {
+      console.error("Error fetching courses:", error);
+      throw error;
+    });
 };
 
 export const StudentService = () => {
