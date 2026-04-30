0c059ba87c2eaff4500eb316da4bcc3369625913
bijayshrestha0817 <bijay.shrestha2@maitriservices.com>
2026-04-30 11:58:23 +0000

    change-password donegit add .git add .!

--- src/app/(auth)/change-password/page.tsx
+++ src/app/(auth)/change-password/page.tsx
@@ -1,23 +1,57 @@
 "use client";
-import { Button, Container, Paper, PasswordInput, Title } from "@mantine/core";
+import { AuthService } from "@/features/auth/services/authAPI";
+import { Button, Container, Notification, Paper, PasswordInput, Title } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
 import classes from "../../../styles/ResetPassword.module.css";
 
+const { changePassword } = AuthService();
 export default function ChangePassword() {
+
+  const router = useRouter()
+
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
   const form = useForm({
     initialValues: {
       current_password: "",
       new_password: "",
     },
   });
+
+  const onSubmit = async (values: typeof form.values) => {
+    setLoading(true)
+    setError(null)
+    try {
+      await changePassword(values.current_password, values.new_password)
+      toast.success("Password changed successfully!")
+      router.push("/dashboard")
+    } catch (err) {
+      console.error(err);
+      setError("Invalid password.");
+    } finally {
+      setLoading(false);
+    }
+
+  }
+
   return (
     <Container size={460} my={30}>
       <Title className={classes.title} ta="center">
         Change your password?
       </Title>
 
+
+      {error && (
+        <Notification color="red" mt="md">
+          {error}
+        </Notification>
+      )}
       <Paper withBorder shadow="sm" p={22} mt={30} radius="md">
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <form onSubmit={form.onSubmit(onSubmit)}>
           <PasswordInput
             label="Current"
             placeholder="Your Current password"
@@ -36,7 +70,7 @@ export default function ChangePassword() {
             {...form.getInputProps("new_password")}
           />
 
-          <Button type="submit" fullWidth mt="xl" radius="md">
+          <Button type="submit" fullWidth mt="xl" radius="md" loading={loading}>
             Confirm Password
           </Button>
         </form>
--- src/features/auth/services/authAPI.ts
+++ src/features/auth/services/authAPI.ts
@@ -1,43 +1,38 @@
+import Cookies from "js-cookie";
 import wretch from "wretch";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_DJANGO_API_URL;
 
 const API_AUTH_URL = process.env.NEXT_PUBLIC_DJANGO_AUTH_API_URL;
-
-const API_URL = wretch(API_BASE_URL)
-  .accept("application/json")
-  .content("application/json");
 
 const AUTH_URL = wretch(API_AUTH_URL)
   .accept("application/json")
   .content("application/json");
 
-const register = (email: string, username: string, password: string) => {
-  return API_URL.post({ email, username, password }, "/auth/users/");
+const getAuth = () => {
+  const token = Cookies.get("access_token");
+
+  return wretch(API_AUTH_URL)
+    .auth(`Bearer ${token}`)
+    .accept("application/json")
+    .content("application/json");
 };
 
-const login = (username: string, password: string) => {
-  return AUTH_URL.post({ username: username, password }, "/auth/token/");
+const changePassword = (current_password: string, new_password: string) => {
+  return getAuth().put({ current_password, new_password }, "/auth/change-password/")
+}
+
+const forgotPassword = (email: string) => {
+  return AUTH_URL.post({ email, }, "/auth/reset-password/");
 };
 
-const handleJWTRefresh = (refreshToken: string) => {
-  return AUTH_URL.post({ refresh: refreshToken }, "/auth/token/refresh/");
-};
-
-const getToken = (key: "access" | "refresh") => {
-  return localStorage.getItem(key);
-};
-
-const storeToken = (token: string, key: "access" | "refresh") => {
-  localStorage.setItem(key, token);
-};
+const forgotPasswordConfirm = (uid: string, token: string, new_password: string) => {
+  return AUTH_URL.post({ uid, token, new_password }, "/auth/forgot-password-confirm/")
+}
 
 export const AuthService = () => {
   return {
-    login,
-    register,
-    handleJWTRefresh,
-    getToken,
-    storeToken,
+    changePassword,
+    forgotPassword,
+    forgotPasswordConfirm
   };
 };
